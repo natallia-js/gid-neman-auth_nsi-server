0b3e6fa0f22b8b38b6c9e36c630430e5a82e8ad1
Natallia <fedartsova@tut.by>
2021-10-28 15:46:53 +0300

    feat: add clear database function

---
+++ api/serverSideProcessing/processDelDBData.js
@@ -0,0 +1,27 @@
+const WorkOrder = require('../models/WorkOrder');
+const Order = require('../models/Order');
+const config = require('config');
+
+
+async function processDelDBData() {
+  // Удаляем те распоряжения из коллекции рабочих распоряжений, которые более не являются действующими
+  const matchFilter = {
+    $and: [
+      { orderChain: { $exists: true } },
+      { "orderChain.chainEndDateTime": { $exists: true } },
+      { "orderChain.chainEndDateTime": { $ne: null } },
+      { "orderChain.chainEndDateTime": { $lte: new Date() } },
+    ]
+  };
+  await WorkOrder.deleteMany(matchFilter);
+
+  // Удаляем данные о распоряжениях, хранящихся в основной коллекции распоряжений более заданного промежутка времени
+  const storeOrdersInDBInMilliseconds = config.get('storeOrdersInDBInDays') * 24 * 60 * 60 * 1000;
+  const delDataUpperDateBorder = new Date();
+  delDataUpperDateBorder.setMilliseconds(delDataUpperDateBorder.getMilliseconds() - storeOrdersInDBInMilliseconds);
+  console.log(delDataUpperDateBorder)
+  await Order.deleteMany({ createDateTime: { $lte: delDataUpperDateBorder } });
+}
+
+
+module.exports = processDelDBData;
