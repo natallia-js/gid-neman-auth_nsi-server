672b94c82cf8da48b29e7c74bb1d44435b9e9c0c
Natallia <fedartsova@tut.by>
2023-02-26 18:59:49 +0300

    feat: process user onDuty status in webSocket client message

--- api/webSocket/setup.js
+++ api/webSocket/setup.js
@@ -14,9 +14,12 @@ const {
 const { addServerActionInfo, addError } = require('../serverSideProcessing/processLogsActions');
 
 
-// accepts an http server instance
+// accepts an http/https server instance
 function setupWebSocket(server) {
-  // websocket server instance
+  // creating a websocket server instance;
+  // 'noServer = true' option says "do not set up an HTTP server alongside this websocket server";
+  // the advantage to doing this is that we can share a single HTTP server (our Express server)
+  // across multiple websocket connections
   const wss = new WebSocket.Server({ noServer: true, clientTracking: true });
 
   // setClientsOnlineStatus(wss.clients);
@@ -24,7 +27,12 @@ function setupWebSocket(server) {
   // hookup broadcast pipeline
   // broadcastPipeline(wss.clients);
 
-  // handle upgrade of the request
+  // We need to handle the attachment of the websocket server to the existing Express Server.
+  // To do it, on the Express Server we listen for an upgrade event.
+  // This event is fired whenever our Express server — a plain HTTP(S) server — receives a request
+  // for an endpoint using the websockets protocol. "Upgrade" here is saying, "we need to upgrade this
+  // request to handle websockets".
+  // Below, we handle upgrade of the request
   // (the WebSocket clients send the HTTP request asking for a WebSocket connection,
   // then the server responds with an HTTP 101 Switching protocols, meaning that it accepts the connection,
   // and then the client can start to send and receive data in binary format)
@@ -36,7 +44,7 @@ function setupWebSocket(server) {
     } catch (err) {
       addError({
         errorTime: new Date(),
-        action: 'ws upgrade',
+        action: `webSocket событие ${UPGRADE_EVENT}`,
         error: err.message,
         actionParams: null,
       });
@@ -57,8 +65,8 @@ function setupWebSocket(server) {
     }
     addServerActionInfo({
       actionTime: new Date(),
-      action: 'новое подключение',
-      description: `подключение с ip=${clientIP}, общее количество подключенных клиентов=${wss.clients.size}`,
+      action: 'Новое подключение',
+      description: `Подключение с ip=${clientIP}, общее количество подключенных клиентов=${wss.clients.size}`,
     });
 
     ctx.isAlive = true;
@@ -79,6 +87,7 @@ function setupWebSocket(server) {
         const clientWorkPoligonType = clientData.length >= 2 ? clientData[1] : null;
         const clientWorkPoligonId = clientData.length >= 3 ? clientData[2] : null;
         const clientWorkSubPoligonId = clientData.length >= 4 ? clientData[3] : null;
+        const isClientOnDuty = clientData.length >= 5 ? clientData[4] : null;
         if (ctx.clientID !== clientID) {
           ctx.clientID = (clientID === 'null') ? null : clientID;
         }
@@ -91,43 +100,59 @@ function setupWebSocket(server) {
         if (ctx.clientWorkSubPoligonId !== clientWorkSubPoligonId) {
           ctx.clientWorkSubPoligonId = (clientWorkSubPoligonId === 'null') ? null : clientWorkSubPoligonId;
         }
+        if (ctx.isClientOnDuty !== isClientOnDuty) {
+          ctx.isClientOnDuty = (isClientOnDuty === 'null') ? null : isClientOnDuty;
+        }
         return;
       }
       // Запрос клиента на получение списка online-пользователей на указанных рабочих полигонах
       if (messageString.match(GET_ONLINE_USERS_MESSAGE_PATTERN)) {
         const workPoligonsObjects = JSON.parse(messageString.slice(7));
         // workPoligonsObjects - массив объектов, каждый из которых состоит из полей type, id, workPlaceId
-        if (Array.isArray(workPoligonsObjects)) {
-          let onlineUsers = [];
-          for (let client of wss.clients.values()) {
-            const wp = workPoligonsObjects.find((wp) =>
-              wp.type === client.clientWorkPoligonType &&
-              String(wp.id) === String(client.clientWorkPoligonId)
-            );
-            if (wp) {
-              const existingItem = onlineUsers.find((el) =>
-                el.type === wp.type &&
-                String(el.id) === String(wp.id) &&
-                (
-                  (!el.workPlaceId && !client.clientWorkSubPoligonId) ||
-                  (el.workPlaceId && client.clientWorkSubPoligonId && String(el.workPlaceId) === String(client.clientWorkSubPoligonId))
-                ));
-              if (existingItem) {
-                if (!existingItem.people.includes(client.clientID)) {
-                  existingItem.people.push(client.clientID);
-                }
-              } else {
-                onlineUsers.push({
-                  type: wp.type,
-                  id: String(wp.id),
-                  workPlaceId: client.clientWorkSubPoligonId ? String(client.clientWorkSubPoligonId): null,
-                  people: [client.clientID],
-                });
-              }
+        if (!Array.isArray(workPoligonsObjects)) {
+          // если workPoligonsObjects - не массив, то нечего обрабатывать
+          return;
+        }
+        // сюда поместим информацию, которую отошлем клиенту по его текущему запросу
+        let onlineUsers = [];
+        // цикл по всем подключившимся к системе пользователям
+        for (let client of wss.clients.values()) {
+          // смотрим, есть ли для очередного пользователя рабочий полигон в списке workPoligonsObjects
+          const wp = workPoligonsObjects.find((wp) =>
+            wp.type === client.clientWorkPoligonType &&
+            String(wp.id) === String(client.clientWorkPoligonId)
+          );
+          // если нет - переходим к следующему пользователю
+          if (!wp) {
+            continue;
+          }
+          // смотрим, помещали ли мы ранее информацию по рабочему полигону wp в массив onlineUsers
+          const existingItem = onlineUsers.find((el) =>
+            el.type === wp.type &&
+            String(el.id) === String(wp.id) &&
+            (
+              (!el.workPlaceId && !client.clientWorkSubPoligonId) ||
+              (el.workPlaceId && client.clientWorkSubPoligonId && String(el.workPlaceId) === String(client.clientWorkSubPoligonId))
+            ));
+          // если информация по рабочему полигону wp есть в onlineUsers и при этом там нет текущего
+          // пользователя, то включаем информацию о нем
+          if (existingItem) {
+            if (!existingItem.people.includes(client.clientID)) {
+              existingItem.people.push({ clientId: client.clientID, isClientOnDuty: client.isClientOnDuty });
             }
           }
-          ctx.send(ONLINE_USERS(onlineUsers));
+          // если информации по рабочему полигону wp нет в onlineUsers, то создаем в onlineUsers
+          // одну запись, относящуюся к wp, с привязкой к ней текущего пользователя
+          else {
+            onlineUsers.push({
+              type: wp.type,
+              id: String(wp.id),
+              workPlaceId: client.clientWorkSubPoligonId ? String(client.clientWorkSubPoligonId): null,
+              people: [{ clientId: client.clientID, isClientOnDuty: client.isClientOnDuty }],
+            });
+          }
         }
+        ctx.send(ONLINE_USERS(onlineUsers));
         return;
       }
     });
